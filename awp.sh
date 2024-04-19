55192a73830e3f850b87ac6274caddf9058facda
ChengDobbs <dobbs.ch26@gmail.com>
2024-04-19 21:09:39 +0800

    update awp shell

--- awp.sh
+++ awp.sh
@@ -21,15 +21,17 @@
 #     --ipc-end 50
 
 iter="2k"
-ipcn=10
+ipcn=50
 folder_path="SRE2L3.0" 
-CUDA_VISIBLE_DEVICES=6 \
+CUDA_VISIBLE_DEVICES=0 \
 python 'recover/recover_in1k_awp.py' \
     --wandb-project $folder_path \
     --wandb-group syn_2k3k_awp \
     --wandb-job-type recover_awp \
     --wandb-name in1k_rn18_awp_${iter}_ipc${ipcn} \
     --exp-name in1k_rn18_awp_${iter}_ipc${ipcn} \
+    --data-select-path data/select_dataset \
+    --select-num 50 \
     --batch-size 100 \
     --lr 0.25 \
     --iteration 2000 \
