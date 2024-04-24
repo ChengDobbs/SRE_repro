bab9531fa7eae730f362d9a490d71a97bdb9445a
ChengDobbs <dobbs.ch26@gmail.com>
2024-04-24 18:01:05 +0800

    update recover tiny

--- recover/recover_tiny.py
+++ recover/recover_tiny.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import random
-
+import wandb
 import numpy as np
 import torch
 import torch.nn as nn
@@ -17,10 +17,61 @@ from utils import BNFeatureHook
 from utils import lr_cosine_policy
 from utils import wandb_init
 
+wandb_metrics = {}
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "recover data from pre-trained model")
+    ''' Data save flags '''
+    parser.add_argument('--exp-name', type=str, default='test',
+                        help='name of the experiment, subfolder under syn_data_path')
+    parser.add_argument('--syn-data-path', type=str,
+                        default='./syn_data', help='where to store synthetic data')
+    parser.add_argument('--store-last-images', action='store_true',
+                        help='whether to store best images')
+    ''' Optimization flags '''
+    parser.add_argument('--batch-size', type=int,
+                        default=100, help='number of images to optimize at the same time')
+    parser.add_argument('--iteration', type=int, default=1000,
+                        help='num of iterations to optimize the synthetic data')
+    parser.add_argument('--lr', type=float, default=0.1,
+                        help='learning rate for optimization')
+    parser.add_argument('--jitter', default=4, type=int, help='random shift on the synthetic data')
+    parser.add_argument('--r-bn', type=float, default=0.05,
+                        help='coefficient for BN feature distribution regularization')
+    parser.add_argument('--first-bn-multiplier', type=float, default=10.,
+                        help='additional multiplier on first bn layer of R_bn')
+    ''' Model flags '''
+    parser.add_argument('--arch-name', type=str, default='resnet18',
+                        help='arch name from pretrained torchvision models')
+    parser.add_argument('--verifier', action='store_true',
+                        help='whether to evaluate synthetic data with another model')
+    parser.add_argument('--verifier-arch', type=str, default='mobilenet_v2',
+                        help="arch name from torchvision models to act as a verifier")
+    parser.add_argument('--arch-path', type=str, default='')
+    parser.add_argument('--verifier-arch-path', type=str, default='')
+    ''' Training Helpers '''
+    parser.add_argument('--ipc-start', default=0, type=int)
+    parser.add_argument('--ipc-end', default=50, type=int)
+
+    '''wandb flags'''
+    parser.add_argument('--wandb-project', type=str, default='SRe2L', 
+                        help='wandb project name')
+    parser.add_argument('--wandb-name', type=str, default='tiny', 
+                        help='name')
+    parser.add_argument('--wandb-group', type=str, default='syn_it3k', 
+                        help='group name')
+    parser.add_argument('--wandb-job-type', type=str, default='recover', help='job type in certain group',
+                        choices=['recover', 'relabel', 'val_KD', 'val_FKD'])
+    
+    args = parser.parse_args()
+    args.syn_data_path = os.path.join(args.syn_data_path, args.exp_name)
+    return args
+
 
 def get_images(args, model_teacher, hook_for_display, ipc_id):
+    global wandb_metrics
     print("generating one IPC images (200)")
-    save_every = 100
     batch_size = args.batch_size
 
     loss_r_feature_layers = []
@@ -33,7 +84,10 @@ def get_images(args, model_teacher, hook_for_display, ipc_id):
     targets_all = torch.LongTensor(np.arange(200))
 
     for kk in range(0, 200, batch_size):
-        targets = targets_all[kk:min(kk + batch_size, 200)].to('cuda')
+                
+        start_index = kk
+        end_index = min(kk + batch_size, 200)
+        targets = targets_all[start_index:end_index].to('cuda')
 
         data_type = torch.float
         inputs = torch.randn((targets.shape[0], 3, 64, 64), requires_grad=True, device='cuda',
@@ -69,12 +123,27 @@ def get_images(args, model_teacher, hook_for_display, ipc_id):
             loss_ce = criterion(outputs, targets)
 
             # R_feature loss
-            rescale = [args.first_bn_multiplier] + [1. for _ in range(len(loss_r_feature_layers) - 1)]
+            rescale = [args.first_bn_multiplier] + [1.]* (len(loss_r_feature_layers) - 1)
             loss_r_bn_feature = sum([mod.r_feature * rescale[idx] for (idx, mod) in enumerate(loss_r_feature_layers)])
 
             # final loss
-            loss = loss_ce + args.r_bn * loss_r_bn_feature
+            loss_aux = args.r_bn * loss_r_bn_feature
+            loss = loss_ce + loss_aux
 
+            metrics = {
+                'syn/loss_ce': loss_ce.item(),
+                'syn/loss_aux': loss_aux.item(),
+                'syn/loss_total': loss.item(),
+                # 'syn/aux_weight': aux_weight,
+                # 'image/acc_image': acc_image,
+                # 'image/loss_image': loss_image,
+                'syn/ipc_id': ipc_id,
+            }
+
+            wandb_metrics.update(metrics)
+            wandb.log(wandb_metrics)
+
+            save_every = args.batch_size
             if iteration % save_every == 0 and args.verifier:
                 print("------------iteration {}----------".format(iteration))
                 print("total loss", loss.item())
@@ -99,6 +168,10 @@ def get_images(args, model_teacher, hook_for_display, ipc_id):
 
 
 def main_syn(ipc_id, args):
+
+    if not os.path.exists(args.syn_data_path):
+        os.makedirs(args.syn_data_path)
+
     model_teacher = models.__dict__[args.arch_name](num_classes=200)
     model_teacher.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
     model_teacher.maxpool = nn.Identity()
@@ -128,50 +201,12 @@ def main_syn(ipc_id, args):
     get_images(args, model_teacher, hook_for_display, ipc_id)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser("SRe2L: recover data from pre-trained model")
-    """ Data save flags """
-    parser.add_argument('--exp-name', type=str, default='test',
-                        help='name of the experiment, subfolder under syn_data_path')
-    parser.add_argument('--syn-data-path', type=str,
-                        default='./syn_data', help='where to store synthetic data')
-    parser.add_argument('--store-last-images', action='store_true',
-                        help='whether to store best images')
-    """ Optimization related flags """
-    parser.add_argument('--batch-size', type=int,
-                        default=100, help='number of images to optimize at the same time')
-    parser.add_argument('--iteration', type=int, default=1000,
-                        help='num of iterations to optimize the synthetic data')
-    parser.add_argument('--lr', type=float, default=0.1,
-                        help='learning rate for optimization')
-    parser.add_argument('--jitter', default=4, type=int, help='random shift on the synthetic data')
-    parser.add_argument('--r-bn', type=float, default=0.05,
-                        help='coefficient for BN feature distribution regularization')
-    parser.add_argument('--first-bn-multiplier', type=float, default=10.,
-                        help='additional multiplier on first bn layer of R_bn')
-    """ Model related flags """
-    parser.add_argument('--arch-name', type=str, default='resnet18',
-                        help='arch name from pretrained torchvision models')
-    parser.add_argument('--verifier', action='store_true',
-                        help='whether to evaluate synthetic data with another model')
-    parser.add_argument('--verifier-arch', type=str, default='mobilenet_v2',
-                        help="arch name from torchvision models to act as a verifier")
-    parser.add_argument('--arch-path', type=str, default='')
-    parser.add_argument('--verifier-arch-path', type=str, default='')
-    """ Training Helpers """
-    parser.add_argument('--ipc-start', default=0, type=int)
-    parser.add_argument('--ipc-end', default=50, type=int)
-    args = parser.parse_args()
-    return args
-
-
 if __name__ == '__main__':
     args = parse_args()
-
-    args.syn_data_path = os.path.join(args.syn_data_path, args.exp_name)
-    if not os.path.exists(args.syn_data_path):
-        os.makedirs(args.syn_data_path)
+    wandb_init(args)
 
     for ipc_id in range(args.ipc_start, args.ipc_end):
         print(f'ipc = {ipc_id}')
         main_syn(ipc_id, args)
+        
+    wandb.finish()
--- tiny.sh
+++ tiny.sh
@@ -3,43 +3,43 @@ folder_path="SRE2L3.0"
 
 bs=256
 ep=50
-python pretrain/squeeze_tiny.py \
-    --wandb-project $folder_path \
-    --wandb-group tiny_awp \
-    --wandb-job-type squeeze \
-    --wandb-name tiny_rn18_awp_bs${bs}_ep${ep} \
-    --model resnet18 \
-    --batch-size $bs \
-    --epochs $ep \
-    --opt sgd \
-    --lr 0.2 \
-    --momentum 0.9 \
-    --weight-decay 1e-4 \
-    --lr-scheduler cosineannealinglr \
-    --lr-warmup-epochs 5 \
-    --lr-warmup-method linear \
-    --lr-warmup-decay 0.01 \
-    --output-dir save/tiny_rn18_ep${ep}
-
-
-# ipcn=50
-# iter=4k
-# python recover/recover_tiny_awp.py \
+# python squeeze/squeeze_tiny.py \
 #     --wandb-project $folder_path \
 #     --wandb-group tiny_awp \
-#     --wandb-job-type recover_awp \
-#     --wandb-name tiny_rn18_awp_${iter}_ipc${ipcn} \
-#     --arch-name resnet18 \
-#     --arch-path save/tiny_rn18_ep${ep}/checkpoint.pth \
-#     --exp-name tiny_rn18_4k_ipc${ipcn} \
-#     --syn-data-path ./syn_data \
-#     --batch-size 100 \
-#     --lr 0.1 \
-#     --r-bn 1 \
-#     --iteration 4000 \
-#     --store-last-images \
-#     --ipc-start 0 \
-#     --ipc-end $ipcn
+#     --wandb-job-type squeeze \
+#     --wandb-name tiny_rn18_awp_bs${bs}_ep${ep} \
+#     --model resnet18 \
+#     --batch-size $bs \
+#     --epochs $ep \
+#     --opt sgd \
+#     --lr 0.2 \
+#     --momentum 0.9 \
+#     --weight-decay 1e-4 \
+#     --lr-scheduler cosineannealinglr \
+#     --lr-warmup-epochs 5 \
+#     --lr-warmup-method linear \
+#     --lr-warmup-decay 0.01 \
+#     --output-dir save/tiny_rn18_ep${ep}
+
+
+ipcn=50
+iter=4k
+python recover/recover_tiny.py \
+    --wandb-project $folder_path \
+    --wandb-group tiny \
+    --wandb-job-type recover \
+    --wandb-name tiny_rn18_awp_${iter}_ipc${ipcn} \
+    --arch-name resnet18 \
+    --arch-path save/tiny_rn18_ep${ep}/checkpoint.pth \
+    --exp-name tiny_rn18_4k_ipc${ipcn} \
+    --syn-data-path ./syn_data \
+    --batch-size 100 \
+    --lr 0.1 \
+    --r-bn 1 \
+    --iteration 4000 \
+    --store-last-images \
+    --ipc-start 0 \
+    --ipc-end $ipcn
 
 
 # python validate/train_kd.py \
