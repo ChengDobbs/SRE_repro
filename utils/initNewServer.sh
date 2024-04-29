e9fbe8a2932e7ba82251eb82bdb7a79a32c18d67
ChengDobbs <dobbs.ch26@gmail.com>
2024-04-29 20:51:11 +0800

    Delete utils directory

--- utils/initNewServer.sh
+++
@@ -1,8 +0,0 @@
-apt purge libhwloc-dev libhwloc-plugins
-pip install datasets huggingface-cli huggingface-hub[hf_transfer] wandb
-
-git clone https://github.com/ChengDobbs/SRe2L_repro.git
-git config --global credential.helper store
-huggingface-cli login
-
-wandb login
