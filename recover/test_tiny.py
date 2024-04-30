431ac52dbd85aa60d0dea82c9edb458d0386a44b
ChengDobbs <dobbs.ch26@gmail.com>
2024-04-30 21:03:51 +0800

    add tiny_test

---
+++ recover/test_tiny.py
@@ -0,0 +1,293 @@
+import argparse
+import os
+import random
+import wandb
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils
+import torch.utils.data.distributed
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+from PIL import Image
+from utils import save_images, validate
+from utils import ImageProcessor
+from utils import BNFeatureHook
+from utils import lr_cosine_policy
+from utils import wandb_init
+from utils import SAM
+
+wandb_metrics = {}
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "recover data from pre-trained model")
+    ''' Data save flags '''
+    parser.add_argument('--exp-name', type=str, default='tiny_rn18_awp_2k_ipc10',
+                        help='name of the experiment, subfolder under syn_data_path')
+    parser.add_argument('--dataset', type=str, default='tinyIN', 
+                        help='type of dataset', choices=['cifar100', 'imagenet', 'tinyIN'])
+    parser.add_argument('--select-num', type=int, default=50,
+                        help='number of images selected for each class')
+    parser.add_argument('--data-select-path', type=str, default='',
+                        help='path to selected images')
+    parser.add_argument('--syn-data-path', type=str,
+                        default='./syn_data', help='where to store synthetic data')
+    parser.add_argument('--store-last-images', action='store_true',
+                        help='whether to store best images')
+    
+    ''' Optimization flags '''
+    parser.add_argument('--batch-size', type=int,
+                        default=100, help='number of images to optimize at the same time')
+    parser.add_argument('--iteration', type=int, default=1000,
+                        help='num of iterations to optimize the synthetic data')
+    parser.add_argument('--lr', type=float, default=0.1,
+                        help='learning rate for optimization')
+    parser.add_argument('--jitter', default=4, type=int, help='random shift on the synthetic data')
+    parser.add_argument('--r-bn', type=float, default=0.05,
+                        help='coefficient for BN feature distribution regularization')
+    parser.add_argument('--first-bn-multiplier', type=float, default=10.,
+                        help='additional multiplier on first bn layer of R_bn')
+    parser.add_argument('--rho', type=float, default=0.015,
+                        help='rho parameter for SAM optimizer')
+    parser.add_argument('--sam-steps', type=int, default=15,
+                        help='number of SAM steps')
+    parser.add_argument('--tanh-space', action='store_true',
+                        help='whether to use tanh space for optimization')
+    
+    ''' Model flags '''
+    parser.add_argument('--arch-name', type=str, default='resnet18',
+                        help='arch name from pretrained torchvision models')
+    parser.add_argument('--verifier', action='store_true',
+                        help='whether to evaluate synthetic data with another model')
+    parser.add_argument('--verifier-arch', type=str, default='mobilenet_v2',
+                        help="arch name from torchvision models to act as a verifier")
+    parser.add_argument('--arch-path', type=str, default='')
+    parser.add_argument('--verifier-arch-path', type=str, default='')
+
+    ''' Training Helpers '''
+    parser.add_argument('--ipc-start', default=0, type=int)
+    parser.add_argument('--ipc-end', default=50, type=int)
+
+    '''wandb flags'''
+    parser.add_argument('--wandb-project', type=str, default='Ortho_SRe2L', 
+                        help='wandb project name')
+    parser.add_argument('--wandb-name', type=str, default='tiny', 
+                        help='name')
+    parser.add_argument('--wandb-group', type=str, default='tiny_awp', 
+                        help='group name')
+    parser.add_argument('--wandb-job-type', type=str, default='recover', help='job type in certain group',
+                        choices=['recover', 'recover_awp', 'relabel', 'val_KD', 'val_FKD'])
+    
+    args = parser.parse_args()
+    args.syn_data_path = os.path.join(args.syn_data_path, args.exp_name)
+    args.data_select_path = os.path.join(args.data_select_path + '_' + str(args.select_num))
+    args.sorted_path = sorted(os.listdir(args.data_select_path))
+    return args
+
+
+def load_image(image_path):
+    image = Image.open(image_path)
+    image = image.convert('RGB')
+    normalize = transforms.Normalize(
+        mean=[0.4802, 0.4481, 0.3975],
+        std=[0.2302, 0.2265, 0.2262]
+    )
+    preprocess = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    return preprocess(image)
+
+
+def get_images(args, model_teacher, hook_for_display, ipc_id):
+    global wandb_metrics
+    print("generating one IPC images (200)")
+    batch_size = args.batch_size
+    dataset = args.dataset
+    loss_r_feature_layers = []
+    for module in model_teacher.modules():
+        if isinstance(module, nn.BatchNorm2d):
+            loss_r_feature_layers.append(BNFeatureHook(module))
+
+    # setup target labels
+    # targets_all = torch.LongTensor(np.random.permutation(200))
+    targets_all = torch.LongTensor(np.arange(200))
+
+    for kk in range(0, 200, batch_size):
+                
+        start_index = kk
+        end_index = min(kk + batch_size, 200)
+        targets = targets_all[start_index:end_index].to('cuda')
+
+        inputs = []
+        for folder_index in range(start_index, end_index):
+            # folder_path = os.path.join(args.data_select_path, os.listdir(args.data_select_path)[folder_index])
+            folder_path = os.path.join(args.data_select_path, args.sorted_path[folder_index])
+            image_path = os.path.join(folder_path, os.listdir(folder_path)[ipc_id])
+            image = load_image(image_path)
+            inputs.append(image)
+
+        img_process = ImageProcessor(dataset)
+        inputs = torch.stack(inputs).to('cuda')
+
+        if args.tanh_space:
+            inputs.data = img_process.denormalize(inputs.data)
+            inputs.data = img_process.inverse_tanh_space(inputs.data)
+
+        inputs.requires_grad = True
+        iterations_per_layer = args.iteration
+        lim_0, lim_1 = args.jitter , args.jitter
+        optimizer = optim.Adam([inputs], lr=args.lr, betas=[0.5, 0.9], eps = 1e-8)
+
+        if args.sam_steps:
+            paras = model_teacher.parameters()
+            sam_optimizer = SAM(paras, optimizer, rho = args.rho / args.sam_steps)
+        
+        if args.tanh_space:
+            inputs.data = img_process.tanh_space(inputs.data)
+            inputs.data = img_process.normalize(inputs.data)
+        
+        lr_scheduler = lr_cosine_policy(args.lr, 0, iterations_per_layer)  # 0 - do not use warmup
+        criterion = nn.CrossEntropyLoss()
+        criterion = criterion.cuda()
+
+        for iteration in range(iterations_per_layer):
+            # learning rate scheduling
+            lr_scheduler(optimizer, iteration, iteration)
+
+            aug_func = transforms.Compose([
+                transforms.RandomResizedCrop(64),
+                transforms.RandomHorizontalFlip(),
+            ])
+            inputs_jit = aug_func(inputs)
+
+            # apply random jitter offsets
+            off1 = random.randint(0, lim_0)
+            off2 = random.randint(0, lim_1)
+            inputs_jit = torch.roll(inputs_jit, shifts=(off1, off2), dims=(2, 3))
+
+            # forward pass
+            optimizer.zero_grad()
+
+            if iteration ==0:
+                for _ in range(args.sam_steps):
+                    outputs = model_teacher(inputs_jit)
+                    loss_ce = criterion(outputs, targets)
+                    loss_ce.backward()
+                    sam_optimizer.first_step(True)
+
+            outputs = model_teacher(inputs_jit)
+
+            # R_cross classification loss
+            loss_ce = criterion(outputs, targets)
+            acc1, acc5 = accuracy(outputs, targets, topk=(1, 5))
+
+            # R_feature loss
+            rescale = [args.first_bn_multiplier] + [1.]* (len(loss_r_feature_layers) - 1)
+            loss_r_bn_feature = [
+                mod.r_feature.to(loss_ce.device) * rescale[idx] for (idx, mod) in enumerate(loss_r_feature_layers)
+            ]
+            loss_r_bn_feature = torch.stack(loss_r_bn_feature).sum()
+            
+            # final loss
+            loss_aux = args.r_bn * loss_r_bn_feature
+            loss = loss_ce + loss_aux
+
+            metrics = {
+                'syn/loss_ce': loss_ce.item(),
+                'syn/loss_aux': loss_aux.item(),
+                'syn/loss_total': loss.item(),
+                'syn/acc1': acc1.item(),
+                'syn/acc5': acc5.item(),
+                'syn/ipc_id': ipc_id,
+            }
+
+            wandb_metrics.update(metrics)
+            wandb.log(wandb_metrics)
+
+            save_every = args.batch_size
+            if iteration % save_every == 0 and args.verifier:
+                print("------------iteration {}----------".format(iteration))
+                print("total loss", loss.item())
+                print("loss_r_bn_feature", loss_r_bn_feature.item())
+                print("main criterion", criterion(outputs, targets).item())
+                # comment below line can speed up the training (no validation process)
+                if hook_for_display is not None:
+                    hook_for_display(inputs, targets)
+
+            # do image update
+            loss.backward()
+            optimizer.step()
+
+            # clip color outlayers
+            inputs.data = img_process.clip(inputs.data)
+
+        if args.store_last_images:
+            best_inputs = inputs.data.clone()  # using multicrop, save the last one
+            best_inputs = img_process.denormalize(best_inputs)
+            save_images(args, best_inputs, targets, ipc_id)
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.inference_mode():
+        maxk = max(topk)
+        batch_size = target.size(0)
+        if target.ndim == 2:
+            target = target.max(dim=1)[1]
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target[None])
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].flatten().sum(dtype=torch.float32)
+            res.append(correct_k * (100.0 / batch_size))
+        return res
+
+def main_syn(ipc_id, args):
+
+    if not os.path.exists(args.syn_data_path):
+        os.makedirs(args.syn_data_path)
+
+    model_teacher = models.__dict__[args.arch_name](num_classes=200)
+    model_teacher.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model_teacher.maxpool = nn.Identity()
+    checkpoint = torch.load(args.arch_path, map_location="cpu")
+    model_teacher.load_state_dict(checkpoint["model"])
+
+    model_teacher = nn.DataParallel(model_teacher).cuda()
+    model_teacher.eval()
+
+    if args.verifier:
+        model_verifier = models.__dict__[args.verifier_arch](num_classes=200)
+        model_verifier.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        model_verifier.maxpool = nn.Identity()
+        checkpoint = torch.load(args.verifier_arch_path, map_location="cpu")
+        model_verifier.load_state_dict(checkpoint["model"])
+
+        model_verifier = model_verifier.cuda()
+        model_verifier.eval()
+        for p in model_verifier.parameters():
+            p.requires_grad = False
+        hook_for_display = lambda x, y: validate(x, y, model_verifier)
+    else:
+        hook_for_display = None
+
+    get_images(args, model_teacher, hook_for_display, ipc_id)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    wandb_init(args)
+
+    for ipc_id in range(args.ipc_start, args.ipc_end):
+        print(f'ipc = {ipc_id}')
+        main_syn(ipc_id, args)
+        
+    wandb.finish()
---
+++ test.sh
@@ -0,0 +1,42 @@
+folder_path="SRE2L3.0"
+
+bs=256
+ep=200
+# python squeeze/squeeze_tiny.py \
+#    --wandb-project $folder_path \
+#    --wandb-group tiny \
+#    --wandb-job-type squeeze \
+#    --wandb-name tiny_rn18_bs${bs}_ep${ep} \
+#    --model resnet18 \
+#    --batch-size $bs \
+#    --epochs $ep \
+#    --opt sgd \
+#    --lr 0.2 \
+#    --momentum 0.9 \
+#    --weight-decay 1e-4 \
+#    --lr-scheduler cosineannealinglr \
+#    --lr-warmup-epochs 5 \
+#    --lr-warmup-method linear \
+#    --lr-warmup-decay 0.01 \
+#    --output-dir save/tiny_rn18_ep${ep}
+
+ipcn=10
+iter=2k
+python recover/test_tiny.py \
+    --wandb-project $folder_path \
+    --wandb-group tiny_awp \
+    --wandb-job-type recover \
+    --wandb-name tiny_rn18_awp_${iter}_ipc${ipcn} \
+    --arch-name resnet18 \
+    --arch-path save/tiny_rn18_ep${ep}/checkpoint.pth \
+    --exp-name tiny_rn18_awp_${iter}_ipc${ipcn} \
+    --data-select-path data/tiny-imagenet-200/select/select_dataset \
+    --select-num 10 \
+    --batch-size 100 \
+    --lr 0.1 \
+    --r-bn 0.01 \
+    --iteration 2000 \
+    --store-last-images \
+    --ipc-start 0 \
+    --ipc-end $ipcn
+
